b95c4e550e0d54e6177bf921e48d172d105046a5
Zhengheng Li <zhenghenge@gmail.com>
2019-01-30 18:44:41 +0800

    train_test.py

--- train_test.py
+++ train_test.py
@@ -9,20 +9,25 @@ from network import Network
 import mnist
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "USAGE: ", sys.argv[0], "<training_data.pkl>"
+    if len(sys.argv) < 3:
+        print "USAGE: ", sys.argv[0], "<training_data.pkl> <test_data.pkl>"
         exit(1)
 
-    pkl_file = sys.argv[1]
+    traning_data_fn = sys.argv[1]
+    test_data_fn = sys.argv[2]
 
     print 'loading image data ...'
-    with open(pkl_file, 'rb') as fin:
+    with open(traning_data_fn, 'rb') as fin:
         image_arrays, image_labels = pkl.load(fin)
+    with open(test_data_fn, 'rb') as fin:
+        image_arrays_t, image_labels_t = pkl.load(fin)
 
     training_data = [mnist.one_training_tuple(image_array, digit)
             for image_array, digit in zip(image_arrays[:50000], image_labels[:50000])]
-    test_data = [mnist.one_training_tuple(image_array, digit)
+    validation_data = [mnist.one_training_tuple(image_array, digit)
             for image_array, digit in zip(image_arrays[50000:], image_labels[50000:])]
+    test_data = [mnist.one_training_tuple(image_array, digit)
+            for image_array, digit in zip(image_arrays_t, image_labels_t)]
 
     print 'training ...'
     net = Network([784, 30, 10])
